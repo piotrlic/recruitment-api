4dd188d3789b9c1061601dfaa467c0c0ddbede85
Piotr Lichwala <piotr.lichwala@tomtom.com>
2022-04-22 11:48:27 +0200

    initial app

---
+++ src/main/java/com/dna/tools/recruitmentapi/RecruitmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.dna.tools.recruitmentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitmentApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dna/tools/recruitmentapi/RecruitmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dna.tools.recruitmentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
